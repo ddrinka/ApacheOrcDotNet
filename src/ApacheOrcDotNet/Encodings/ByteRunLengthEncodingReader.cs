f23cf99490f720419aaa57f6482e763d3c153bfa
Douglas Drinka <ddrinka@gmail.com>
2017-01-25 14:13:44 -0600

    Added ByteRunLengthEncodingReader and tests.

---
+++ src/ApacheOrcDotNet/Encodings/ByteRunLengthEncodingReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApacheOrcDotNet.Encodings
+{
+    public class ByteRunLengthEncodingReader
+    {
+		readonly Stream _inputStream;
+
+		public ByteRunLengthEncodingReader(Stream inputStream)
+		{
+			_inputStream = inputStream;
+		}
+
+		public IEnumerable<byte> Read()
+		{
+			while(true)
+			{
+				int firstByte = _inputStream.ReadByte();
+				if (firstByte < 0)  //No more data available
+					yield break;
+
+				if(firstByte < 0x80)    //A run
+				{
+					var numBytes = firstByte + 3;
+					var repeatedByte = _inputStream.CheckedReadByte();
+					for (int i = 0; i < numBytes; i++)
+						yield return repeatedByte;
+				}
+				else  //Literals
+				{
+					var numBytes = 0x100 - firstByte;
+					for (int i = 0; i < numBytes; i++)
+						yield return _inputStream.CheckedReadByte();
+				}
+			}
+		}
+    }
+}
