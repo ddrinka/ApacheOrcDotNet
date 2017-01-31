04bc0db053c06df117391e021fc2f7cc90c31059
Douglas Drinka <ddrinka@gmail.com>
2017-01-31 14:55:42 -0600

    Corrected DecimalStatistics' type to use Strings.

--- src/ApacheOrcDotNet/Statistics/DecimalStatistics.cs
+++ src/ApacheOrcDotNet/Statistics/DecimalStatistics.cs
@@ -9,8 +9,11 @@ namespace ApacheOrcDotNet.Statistics
 	[ProtoContract]
 	public class DecimalStatistics : IDecimalStatistics
 	{
-		[ProtoMember(1)] public decimal Minimum { get; set; }
-		[ProtoMember(2)] public decimal Maximum { get; set; }
-		[ProtoMember(3)] public decimal Sum { get; set; }
+		[ProtoMember(1)] public string Minimum { get; set; }
+		decimal IDecimalStatistics.Minimum => decimal.Parse(Minimum);
+		[ProtoMember(2)] public string Maximum { get; set; }
+		decimal IDecimalStatistics.Maximum => decimal.Parse(Maximum);
+		[ProtoMember(3)] public string Sum { get; set; }
+		decimal IDecimalStatistics.Sum => decimal.Parse(Sum);
 	}
 }
