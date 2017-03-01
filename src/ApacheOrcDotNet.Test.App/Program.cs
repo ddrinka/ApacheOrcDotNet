561eec71d6cf9f8e26e1c946215161ac1b802635
Douglas Drinka <ddrinka@gmail.com>
2017-03-01 13:51:36 -0600

    Added a tool to generate test ORC files.

---
+++ src/ApacheOrcDotNet.Test.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApacheOrcDotNet.Test.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			var random = new Random(123);
+			var testElements = new List<TestClass>();
+			for (int i = 0; i < 70000; i++)
+				testElements.Add(new TestClass { Column1 = random.Next() });
+
+			using (var fileStream = new FileStream("test.orc", FileMode.Create, FileAccess.Write))
+			using (var writer = new OrcWriter<TestClass>(fileStream, new WriterConfiguration())) //Use the default configuration
+			{
+				writer.AddRows(testElements);
+			}
+		}
+	}
+
+	class TestClass
+	{
+		public int Column1 { get; set; }
+	}
+}
