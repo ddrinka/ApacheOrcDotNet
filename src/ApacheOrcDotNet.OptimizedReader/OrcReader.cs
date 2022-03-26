3e64abaa03f01b622204c21e152a46f117cd1adb
Douglas Drinka <ddrinka@gmail.com>
2022-03-26 04:18:39 -0600

    Initial work on index search.

--- src/ApacheOrcDotNet.OptimizedReader/OrcReader.cs
+++ src/ApacheOrcDotNet.OptimizedReader/OrcReader.cs
@@ -1,4 +1,5 @@
 ﻿using ApacheOrcDotNet.Protocol;
+using ApacheOrcDotNet.Statistics;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
@@ -24,12 +25,15 @@ namespace ApacheOrcDotNet.OptimizedReader
     {
         readonly OrcReaderConfiguration _configuration;
         readonly IByteRangeProvider _byteRangeProvider;
-        SpanFileTail _fileTail = null;
+        readonly SpanFileTail _fileTail;
+        readonly Dictionary<int, RowIndex> _stripeIndices = new();
 
         public OrcReader(OrcReaderConfiguration configuration, IByteRangeProvider byteRangeProvider)
         {
             _configuration = configuration;
             _byteRangeProvider = byteRangeProvider;
+
+            _fileTail = ReadFileTail();
         }
 
         public IEnumerable<OrcReaderResultSet<T>> Search<T>(params ColumnDescriptor[] columns) where T : struct
@@ -37,24 +41,53 @@ namespace ApacheOrcDotNet.OptimizedReader
             throw new NotImplementedException();
         }
 
-        void EnsureFileTailRead()
+        bool FileContainsData(params ColumnDescriptor[] columns)
         {
-            if (_fileTail != null)
-                return;
+            foreach (var descriptor in columns)
+            {
+                var (columnId, columnType) = LookUpColumn(descriptor);
+                var stats = _fileTail.Footer.Statistics[columnId];
+                if (!stats.InRange(columnType, descriptor.Min, descriptor.Max))
+                    return false;
+            }
+            return true;
+        }
 
+        (int columnId, ColumnTypeKind columnType) LookUpColumn(ColumnDescriptor descriptor)
+        {
+            var columnId = _fileTail.Footer.Types[0].FieldNames.FindIndex(fn => fn.ToLower() == descriptor.ColumnName.ToLower()) + 1;
+            if (columnId == -1)
+                throw new KeyNotFoundException($"'{descriptor.ColumnName}' not found in ORC data");
+            var columnType = _fileTail.Footer.Types[columnId].Kind;
+
+            return (columnId, columnType);
+        }
+
+        SpanFileTail ReadFileTail()
+        {
             int lengthToReadFromEnd = _configuration.OptimisticFileTailReadLength;
             while (true)
             {
                 var buffer = ArrayPool<byte>.Shared.Rent(lengthToReadFromEnd);
                 _byteRangeProvider.GetRangeFromEnd(buffer, lengthToReadFromEnd);
-                var success = SpanFileTail.TryRead(buffer, out _fileTail, out var additionalBytesRequired);
+                var success = SpanFileTail.TryRead(buffer, out var fileTail, out var additionalBytesRequired);
                 ArrayPool<byte>.Shared.Return(buffer);
 
                 if (success)
-                    break;
+                    return fileTail;
 
                 lengthToReadFromEnd += additionalBytesRequired;
             }
+        }
+
+        void EnsureStripeIndexRead(int stripeId)
+        {
+            if (_stripeIndices.ContainsKey(stripeId))
+                return;
+
+            var stripe = _fileTail.Footer.Stripes[stripeId];
+            var stripeIndexStart = stripe.Offset;
+            var stripIndexLength = stripe.IndexLength;
         }
     }
 }
