0da18ad3aa057b00b3f097dc159a5bca31f0929f
akasarto <5660350+akasarto@users.noreply.github.com>
2022-07-12 16:27:52 -0300

    Add loop unroll benchmark.

---
+++ perf/MicroBenchmarks/LoopUnrollBenchmarks.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace MicroBenchmarks
+{
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class LoopUnrollBenchmarks
+    {
+        [Benchmark]
+        public void NormalLoop()
+        {
+            var numValuesRead = 0;
+            var numByteValuesRead = 1000;
+            Span<bool> outputValues = stackalloc bool[numByteValuesRead * 8];
+
+            for (int idx = 0; idx < numByteValuesRead; idx++)
+            {
+                var decodedByte = 0xff;
+
+                for (int bitIdx = 7; bitIdx >= 0; bitIdx--)
+                {
+                    outputValues[numValuesRead++] = (decodedByte & 1 << bitIdx) != 0;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void UnrolledLoop()
+        {
+            var numValuesRead = 0;
+            var numByteValuesRead = 1000;
+            Span<bool> outputValues = stackalloc bool[numByteValuesRead * 8];
+
+            for (int idx = 0; idx < numByteValuesRead; idx++)
+            {
+                var decodedByte = 0xff;
+
+                outputValues[numValuesRead++] = (decodedByte & 128) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 64) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 32) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 16) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 8) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 4) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 2) != 0;
+                outputValues[numValuesRead++] = (decodedByte & 1) != 0;
+            }
+        }
+    }
+}
