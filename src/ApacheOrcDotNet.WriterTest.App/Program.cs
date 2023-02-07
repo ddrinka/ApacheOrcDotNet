0a7cab5e6a1949efc5c597c41ee251c9104afcaf
Ericvf <ericvf@fegelein.com>
2023-02-07 21:48:50 +0100

    Added booleans and nullable booleans to the writer app. Fixed presenter also requires continuous bitwriter

--- src/ApacheOrcDotNet.WriterTest.App/Program.cs
+++ src/ApacheOrcDotNet.WriterTest.App/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using ApacheOrcDotNet.FluentSerialization;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using ApacheOrcDotNet.FluentSerialization;
 
 namespace ApacheOrcDotNet.Test.App
 {
@@ -11,58 +9,66 @@ namespace ApacheOrcDotNet.Test.App
     {
         public static void Main(string[] args)
         {
-			var baseTime = new DateTime(2017, 3, 16, 0, 0, 0, DateTimeKind.Utc);
-			var rand = new Random(123);
-			var testElements = new List<TestClass>();
-			for (int i = 0; i < 80000; i++)
-			{
-				var random = rand.Next();
-				var set = i / 10000;
-				var randomInRange = (random % 10000) + set * 10000 - 40000;
-				var dec = (DateTime.Now - DateTime.Today).Ticks / (decimal)TimeSpan.TicksPerSecond;
-				var timestamp = baseTime.AddTicks(random);
-				var element = new TestClass
-				{
-					Random = random,
-					RandomInRange = randomInRange,
-					Incrementing = i,
-					SetNumber = set,
-					Double = (double)i / (set + 1),
-					Float = (float)i / (set + 1),
-					Dec = dec,
-					Timestamp = timestamp,
-					Str = $"Random={random}, RandomInRange={randomInRange}, Incrementing={i}, SetNumber={set}, Dec={dec}, Timestamp={timestamp:MM/dd/yyyy hh:mm:ss.fffffff}",
-					DictionaryStr = $"SetNumber={set}"
-				};
-				testElements.Add(element);
-			}
+            var baseTime = new DateTime(2017, 3, 16, 0, 0, 0, DateTimeKind.Utc);
+            var rand = new Random(123);
+            var testElements = new List<TestClass>();
+            var boolToggler = false;
+            for (int i = 0; i < 1000; i++)
+            {
+                var random = rand.Next();
+                var set = i / 10000;
+                var randomInRange = (random % 10000) + set * 10000 - 40000;
+                var dec = (DateTime.Now - DateTime.Today).Ticks / (decimal)TimeSpan.TicksPerSecond;
+                var timestamp = baseTime.AddTicks(random);
+                var element = new TestClass
+                {
+                    Random = random,
+                    RandomInRange = randomInRange,
+                    Incrementing = i,
+                    SetNumber = set,
+                    Double = (double)i / (set + 1),
+                    Float = (float)i / (set + 1),
+                    Dec = dec,
+                    Timestamp = timestamp,
+                    Str = $"Random={random}, RandomInRange={randomInRange}, Incrementing={i}, SetNumber={set}, Dec={dec}, Timestamp={timestamp:MM/dd/yyyy hh:mm:ss.fffffff}",
+                    DictionaryStr = $"SetNumber={set}",
+                    Boolean = boolToggler,
+                    NullBooleans = boolToggler ? null : true,
+                };
 
-			var serializationConfiguration = new SerializationConfiguration()
-					.ConfigureType<TestClass>()
-						.ConfigureProperty(x => x.Dec, x => { x.DecimalPrecision = 14; x.DecimalScale = 9; })
-						.Build();
+                boolToggler = !boolToggler;
 
-			using (var fileStream = new FileStream("test.orc", FileMode.Create, FileAccess.Write))
-			using (var writer = new OrcWriter<TestClass>(fileStream, new WriterConfiguration(), serializationConfiguration)) //Use the default configuration
-			{
-				writer.AddRows(testElements);
-			}
-		}
-	}
+                testElements.Add(element);
+            }
 
-	class TestClass
-	{
-		public int Random { get; set; }
-		public int RandomInRange { get; set; }
-		public int Incrementing { get; set; }
-		public int SetNumber { get; set; }
-		public int? AllNulls { get; set; }
-		public double Double { get; set; }
-		public float Float { get; set; }
-		public decimal Dec { get; set; }
-		public decimal? AllNullsDec { get; set; }
-		public DateTime Timestamp { get; set; }
-		public string Str { get; set; }
-		public string DictionaryStr { get; set; }
-	}
+            var serializationConfiguration = new SerializationConfiguration()
+                    .ConfigureType<TestClass>()
+                        .ConfigureProperty(x => x.Dec, x => { x.DecimalPrecision = 14; x.DecimalScale = 9; })
+                        .Build();
+
+            using (var fileStream = new FileStream("test.orc", FileMode.Create, FileAccess.Write))
+            using (var writer = new OrcWriter<TestClass>(fileStream, new WriterConfiguration() {  RowIndexStride = 10 }, serializationConfiguration)) //Use the default configuration
+            {
+                writer.AddRows(testElements);
+            }
+        }
+    }
+
+    class TestClass
+    {
+        public int Random { get; set; }
+        public int RandomInRange { get; set; }
+        public int Incrementing { get; set; }
+        public int SetNumber { get; set; }
+        public int? AllNulls { get; set; }
+        public double Double { get; set; }
+        public float Float { get; set; }
+        public decimal Dec { get; set; }
+        public decimal? AllNullsDec { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Str { get; set; }
+        public string DictionaryStr { get; set; }
+        public bool Boolean { get; set; }
+        public bool? NullBooleans { get; set; }
+    }
 }
--- src/ApacheOrcDotNet/ColumnTypes/BooleanWriter.cs
+++ src/ApacheOrcDotNet/ColumnTypes/BooleanWriter.cs
@@ -1,93 +1,98 @@
 ﻿using ApacheOrcDotNet.Compression;
-using ApacheOrcDotNet.Encodings;
 using ApacheOrcDotNet.Protocol;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApacheOrcDotNet.ColumnTypes
 {
     public class BooleanWriter : IColumnWriter<bool?>
     {
-		readonly bool _isNullable;
-		readonly OrcCompressedBuffer _presentBuffer;
-		readonly OrcCompressedBuffer _dataBuffer;
-		private readonly ContinuousBitWriter _bitWriter;
+        readonly bool _isNullable;
+        readonly OrcCompressedBuffer _presentBuffer;
+        readonly OrcCompressedBuffer _dataBuffer;
+        private readonly ContinuousBitWriter _dataWriter;
+        private readonly ContinuousBitWriter _presentWriter;
 
-		public BooleanWriter(bool isNullable, OrcCompressedBufferFactory bufferFactory, uint columnId)
-		{
-			_isNullable = isNullable;
-			ColumnId = columnId;
+        public BooleanWriter(bool isNullable, OrcCompressedBufferFactory bufferFactory, uint columnId)
+        {
+            _isNullable = isNullable;
+            ColumnId = columnId;
 
-			if (_isNullable)
-			{
-				_presentBuffer = bufferFactory.CreateBuffer(StreamKind.Present);
-				_presentBuffer.MustBeIncluded = false;
-			}
-			_dataBuffer = bufferFactory.CreateBuffer(StreamKind.Data);
-			_bitWriter = new ContinuousBitWriter(_dataBuffer);
-		}
+            if (_isNullable)
+            {
+                _presentBuffer = bufferFactory.CreateBuffer(StreamKind.Present);
+                _presentBuffer.MustBeIncluded = false;
+                _presentWriter = new ContinuousBitWriter(_presentBuffer);
+            }
+            _dataBuffer = bufferFactory.CreateBuffer(StreamKind.Data);
+            _dataWriter = new ContinuousBitWriter(_dataBuffer);
 
-		public List<IStatistics> Statistics { get; } = new List<IStatistics>();
-		public long CompressedLength => Buffers.Sum(s => s.Length);
-		public uint ColumnId { get; }
-		public OrcCompressedBuffer[] Buffers => _isNullable ? new[] { _presentBuffer, _dataBuffer } : new[] { _dataBuffer };
-		public ColumnEncodingKind ColumnEncoding => ColumnEncodingKind.Direct;
+        }
 
-		public void FlushBuffers()
-		{
-			_bitWriter.Flush();
-			foreach (var buffer in Buffers)
-				buffer.Flush();
-		}
+        public List<IStatistics> Statistics { get; } = new List<IStatistics>();
+        public long CompressedLength => Buffers.Sum(s => s.Length);
+        public uint ColumnId { get; }
+        public OrcCompressedBuffer[] Buffers => _isNullable ? new[] { _presentBuffer, _dataBuffer } : new[] { _dataBuffer };
+        public ColumnEncodingKind ColumnEncoding => ColumnEncodingKind.Direct;
 
-		public void Reset()
-		{
-			foreach (var buffer in Buffers)
-				buffer.Reset();
-			if(_isNullable)
-				_presentBuffer.MustBeIncluded = false;
-			Statistics.Clear();
-		}
+        public void FlushBuffers()
+        {
+            if (_isNullable)
+            {
+                _presentWriter.Flush();
+            }
 
-		public void AddBlock(IList<bool?> values)
-		{
-			var stats = new BooleanWriterStatistics();
-			Statistics.Add(stats);
+            _dataWriter.Flush();
+
+            foreach (var buffer in Buffers)
+                buffer.Flush();
+        }
+
+        public void Reset()
+        {
+            foreach (var buffer in Buffers)
+                buffer.Reset();
+            if (_isNullable)
+                _presentBuffer.MustBeIncluded = false;
+            Statistics.Clear();
+        }
+
+        public void AddBlock(IList<bool?> values)
+        {
+            var stats = new BooleanWriterStatistics();
+            Statistics.Add(stats);
             if (_isNullable)
                 _presentBuffer.AnnotatePosition(stats, rleValuesToConsume: 0, bitsToConsume: 0);
             _dataBuffer.AnnotatePosition(stats, rleValuesToConsume: 0, bitsToConsume: 0);
 
-			var valList = new List<bool>(values.Count);
+            var valList = new List<bool>(values.Count);
 
-			if(_isNullable)
-			{
-				var presentList = new List<bool>(values.Count);
+            if (_isNullable)
+            {
+                var presentList = new List<bool>(values.Count);
 
-				foreach(var value in values)
-				{
-					stats.AddValue(value);
-					if (value.HasValue)
-						valList.Add(value.Value);
-					presentList.Add(value.HasValue);
-				}
+                foreach (var value in values)
+                {
+                    stats.AddValue(value);
+                    if (value.HasValue)
+                        valList.Add(value.Value);
+                    presentList.Add(value.HasValue);
+                }
 
-				var presentEncoder = new BitWriter(_presentBuffer);
-				presentEncoder.Write(presentList);
-				if (stats.HasNull)
-					_presentBuffer.MustBeIncluded = true;
-			}
-			else
-			{
-				foreach(var value in values)
-				{
-					stats.AddValue(value);
-					valList.Add(value.Value);
-				}
-			}
+                _presentWriter.Write(presentList);
+                if (stats.HasNull)
+                    _presentBuffer.MustBeIncluded = true;
+            }
+            else
+            {
+                foreach (var value in values)
+                {
+                    stats.AddValue(value);
+                    valList.Add(value.Value);
+                }
+            }
 
-			_bitWriter.Write(valList);
-		}
-	}
+            _dataWriter.Write(valList);
+        }
+    }
 }
